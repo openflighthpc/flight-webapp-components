5fb5fe9e0bb52bfa63bf22f0a9ca3557476eb238
Ben Armston <ben.armston@alces-software.com>
2021-08-05 19:17:40 +0100

    404 page styled similarly to landing page
    A similarly styled page will help the landing page and webapps feel more
    like an integrated suite.  There is much more to do to achieve that end.

--- es/index.js
+++ es/index.js
@@ -2,21 +2,22 @@ import * as _utils from './utils';
 export { _utils as utils };
 export * from './Branding';
 export * from './BrandingContext';
-export { default as ErrorBoundary } from './ErrorBoundary';
 export * from './ErrorBoundary';
+export * from './FetchProvider';
 export * from './Overlay';
+export * from './Spinner';
 export * from './useMedia';
 export { Context as ConfigContext, Provider as ConfigProvider } from './ConfigContext';
 export { default as AnimatedRouter } from './AnimatedRouter';
 export { default as AuthenticatedRoute } from './AuthenticatedRoute';
 export { default as BrandBar } from './BrandBar';
 export { default as ConfirmedActionButton } from './ConfirmedActionButton';
+export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as FetchProvider } from './FetchProvider';
-export * from './FetchProvider';
 export { default as Footer } from './Footer';
 export { default as FullscreenButton } from './FullscreenButton';
+export { default as NotFound } from './NotFound';
 export { default as Spinner } from './Spinner';
-export * from './Spinner';
 export { default as UnauthorizedError } from './UnauthorizedError';
 export { default as useEventListener } from './useEventListener'; // Export a subset of the account modules.
 
