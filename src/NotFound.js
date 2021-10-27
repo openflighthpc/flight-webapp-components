cd850eae4b5957f98d74b82267fff7c6228f4be0
Ben Armston <ben.armston@alces-software.com>
2021-10-27 11:57:49 +0100

    Fix NotFound home link

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function NotFound({ homeLink }) {
-  const HomeLink = homeLink;
   return (
     <div className="page-wrap d-flex flex-row align-items-center">
       <div className="container">
@@ -9,7 +8,7 @@ function NotFound({ homeLink }) {
           <div className="col-md-12 text-center">
             <span className="display-1 d-block">404</span>
             <div className="mb-4 lead">This is not the page you are looking for!</div>
-            <HomeLink />
+            {homeLink}
           </div>
         </div>
       </div>
