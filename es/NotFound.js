3102166a177dc68447ad213eac24aa30030d285b
Ben Armston <ben.armston@alces-software.com>
2021-10-27 11:57:56 +0100

    Build artefacts

--- es/NotFound.js
+++ es/NotFound.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 function NotFound(_ref) {
   var homeLink = _ref.homeLink;
-  var HomeLink = homeLink;
   return /*#__PURE__*/React.createElement("div", {
     className: "page-wrap d-flex flex-row align-items-center"
   }, /*#__PURE__*/React.createElement("div", {
@@ -15,7 +14,7 @@ function NotFound(_ref) {
     className: "display-1 d-block"
   }, "404"), /*#__PURE__*/React.createElement("div", {
     className: "mb-4 lead"
-  }, "This is not the page you are looking for!"), /*#__PURE__*/React.createElement(HomeLink, null)))));
+  }, "This is not the page you are looking for!"), homeLink))));
 }
 
 export default NotFound;
