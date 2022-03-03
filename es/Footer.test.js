3e3c71b94ef9b17cde9ed7287aa6141532db6644
Ben Armston <ben.armston@alces-software.com>
2022-03-03 11:09:51 +0000

    Remove old test artefacts

--- es/Footer.test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Footer from './Footer';
-test('renders without crashing', function () {
-  render( /*#__PURE__*/React.createElement(Footer, null));
-});
