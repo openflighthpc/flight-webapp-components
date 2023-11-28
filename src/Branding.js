fa5c8bc9c35a22456a149c5388c5f0dffeafa5b7
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2023-11-28 16:39:57 +0000

    Make footer consistent with landing page

--- src/Branding.js
+++ src/Branding.js
@@ -2,7 +2,10 @@ import React from 'react';
 import classNames from 'classnames';
 
 import DefaultBrandbarLogo from '../dist/images/openflighthpc_nav.png';
+import DefaultFooterLogo from '../dist/images/openflighthpc_footer.png';
 import PoweredByAlces from '../dist/images/poweredby_white.png';
+import LeftCloud from '../dist/images/parting_cloud_l.png';
+import RightCloud from '../dist/images/parting_cloud_r.png';
 import { useBranding } from './BrandingContext';
 
 export function BrandbarLogo() {
@@ -21,12 +24,48 @@ export function BrandbarLogo() {
   );
 }
 
+export function FooterLogo() {
+  const branding = useBranding();
+  const logo = branding('brandbar.logo') || {
+    url: DefaultFooterLogo,
+    alt: "OpenflightHPC Logo",
+  };
+
+  return (
+    <img
+      alt={logo.alt}
+      id="footer-logo"
+      src={logo.url}
+    />
+  );
+}
+
 export function PoweredByLogo() {
   return (
     <img
       alt="Powered by Alces Flight"
       className='branding-brandbar-logo'
       src={PoweredByAlces}
+    />
+  );
+}
+
+export function Cloud(props) {
+  const side = props['side'];
+  const id = `cloud-${side}`;
+  let src;
+  if (side === 'l') {
+    src = LeftCloud;
+  } else {
+    src = RightCloud;
+  }
+
+  return (
+    <img
+      alt=""
+      id={id}
+      className='cloud'
+      src={src}
     />
   );
 }
