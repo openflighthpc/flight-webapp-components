fb05bc9e4b47fb4ec1e7abea6f88924ae09c96c5
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2023-11-29 16:26:53 +0000

    Change login button styling

--- src/account/SignedOut.js
+++ src/account/SignedOut.js
@@ -2,13 +2,12 @@ import React from 'react'
 
 export default function SignedOut({ onClick }) {
   return (
-    <button
-      className="ml-3 btn btn-success mr-1 pl-3 pr-4 text-uppercase font-weight-bold"
+    <a
+      className="button link"
       type="submit"
       onClick={onClick}
     >
-      <i className="px-1 fa fa-user"></i>
-      Log in
-    </button>
+      LOG IN
+    </a>
   );
 }
