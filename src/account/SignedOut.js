29938e6daa12380cdece1412714d7dee4285d42d
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2023-12-01 17:57:13 +0000

    Fix login button styling

--- src/account/SignedOut.js
+++ src/account/SignedOut.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function SignedOut({ onClick }) {
   return (
-    <a
-      className="button link"
-      type="submit"
-      onClick={onClick}
-    >
-      LOG IN
-    </a>
+    <li className="nav-item">
+      <a
+        className="button link mx-4"
+        type="submit"
+        onClick={onClick}
+      >
+        LOG IN
+      </a>
+    </li>
   );
 }
